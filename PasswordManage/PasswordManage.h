32e574b1cbcf01781ba4260ee7c4284c3876b21e
N1neSun <917549681@qq.com>
2022-01-17 21:58:10 +0800

    init

---
+++ PasswordManage/PasswordManage.h
@@ -0,0 +1,32 @@
+﻿
+// PasswordManage.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPasswordManageApp:
+// 有关此类的实现，请参阅 PasswordManage.cpp
+//
+
+class CPasswordManageApp : public CWinApp
+{
+public:
+	CPasswordManageApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPasswordManageApp theApp;
