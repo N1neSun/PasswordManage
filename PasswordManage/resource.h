62d30c4c1f4f6c607f42a02356fcc92a05d77bcf
N1neSun <917549681@qq.com>
2022-01-22 20:58:15 +0800

    add right menu

--- PasswordManage/resource.h
+++ PasswordManage/resource.h
@@ -7,16 +7,22 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_PASSWORDMANAGE_DIALOG       102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
 #define IDC_LIST_PWMANAGE               1001
 #define IDC_BUTTON_SAVE                 1002
-#define IDC_LIST2                       1004
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_MENU_ADD                     32773
+#define ID_MENU_DELETE                  32774
+#define ID_32775                        32775
+#define ID_MENU_EDIT                    32776
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
