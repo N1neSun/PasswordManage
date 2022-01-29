26315f34956d836092569a14370a42987599e745
N1neSun <917549681@qq.com>
2022-01-29 11:54:39 +0800

    add table

--- PasswordManage/SqliteDatabase.cpp
+++ PasswordManage/SqliteDatabase.cpp
@@ -2,3 +2,12 @@
 
 #define DATABASE "PasswordDB.db"
 #define MASTER_TABLE "PasswordInfo"
+
+static struct TableColumnInfo PASSWORD_TABLE_COLUMNS[] = {
+	{"id", "int", "NOT NULL PRIMARY KEY"},
+	{"strName", "varchar(512)", "NOT NULL PRIMARY KEY"},
+	{"strUsername", "varchar(512)", "NOT NULL"},
+	{"strPassword", "varchar(512)", "NOT NULL"},
+	{"strUrl", "varchar(512)", "NOT NULL DEFAULT 100"},
+	{"strNotes", "text", "NOT NULL"}
+};
