0b3dadf42dbb3981a4a76a363d2f59fcd2577072
N1neSun <917549681@qq.com>
2022-04-14 23:46:56 +0800

    overwrite CSetRandomPasswordDlg

--- PasswordManage/CSetRandomPasswordDlg.cpp
+++ PasswordManage/CSetRandomPasswordDlg.cpp
@@ -53,24 +53,9 @@ void CSetRandomPassword::OnBnClickedButtonRandom()
 	// TODO: 在此添加控件通知处理程序代码
 	UpdateData(TRUE);
 	std::string strMark;
-	if (m_IsNum)
-		strMark += "1";
-	else
-		strMark += "0";
-	if (m_IsChar)
-		strMark += "1";
-	else
-		strMark += "0";
-	if (m_IsSymbol)
-		strMark += "1";
-	else
-		strMark += "0";
-	if (m_IsLower)
-		strMark += "1";
-	else
-		strMark += "0";
-	if (m_IsUpper)
-		strMark += "1";
-	else
-		strMark += "0";
+	m_IsNum ? strMark += "1" : strMark += "0";
+	m_IsChar ? strMark += "1" : strMark += "0";
+	m_IsSymbol ? strMark += "1" : strMark += "0";
+	m_IsLower ? strMark += "1" : strMark += "0";
+	m_IsUpper ? strMark += "1" : strMark += "0";
 }
