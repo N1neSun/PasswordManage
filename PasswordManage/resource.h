affcd7db00cb2a9ecca75c5aab29d69862ec1675
N1neSun <917549681@qq.com>
2022-05-22 23:57:28 +0800

    add menu

--- PasswordManage/resource.h
+++ PasswordManage/resource.h
@@ -50,13 +50,15 @@
 #define ID_COPYUSERNAME                 32778
 #define ID_32779                        32779
 #define ID_COPYPASSWORD                 32780
+#define ID_Menu                         32781
+#define ID_Menu_SYSNC                   32782
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        141
-#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_COMMAND_VALUE         32783
 #define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
